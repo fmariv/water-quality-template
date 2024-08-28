b1ec613c1b585d06e8ed553e40fd0056329d0d6c
Fran Martin <fran@earthpulse.ai>
2024-08-28 16:42:39 +0200

    Add try-except

--- scripts/water-quality/main.py
+++ scripts/water-quality/main.py
@@ -12,13 +12,18 @@ vars = SPAIVars()
 
 
 if __name__ == "__main__":
-    collection = "sentinel-2-l2a"
-    images = storage.list(f"{collection}*.tif")
-    aoi = vars["AOI"]
+    try:
+        collection = "sentinel-2-l2a"
+        images = storage.list(f"{collection}*.tif")
+        aoi = vars["AOI"]
 
-    if not images:
-        raise ValueError("No images found for the given AoI. The process will stop.")
+        if not images:
+            raise ValueError(
+                "No images found for the given AoI. The process will stop."
+            )
 
-    for image in tqdm(images, desc="Processing images..."):
-        date = image.split("_")[1].split(".")[0]  # Extract date from image name
-        water_quality(image, date, storage)
+        for image in tqdm(images, desc="Processing images..."):
+            date = image.split("_")[1].split(".")[0]  # Extract date from image name
+            water_quality(image, date, storage)
+    except Exception as e:
+        print(f"An error occurred: {e}. The process will stop.")
